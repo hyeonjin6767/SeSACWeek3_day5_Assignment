76583d19af5c43175b35f8a17fc20bb00b1fb6c3
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-19 14:40:07 +0900

    ViewController+CollectionView를 위한 셋팅

---
+++ SeSACWeek3_day5_Assignment/TRAVEL_TALK_scene/ChattingDataCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingDataCollectionViewCell.swift
+//  SeSACWeek3_day5_Assignment
+//
+//  Created by 박현진 on 7/19/25.
+//
+
+import UIKit
+
+class ChattingDataCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SeSACWeek3_day5_Assignment/TRAVEL_TALK_scene/ChattingDataViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChattingDataViewController.swift
+//  SeSACWeek3_day5_Assignment
+//
+//  Created by 박현진 on 7/19/25.
+//
+
+import UIKit
+
+class ChattingDataViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+
+    @IBOutlet var chatListCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+
+}
